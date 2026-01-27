a1da491811d1551904bc5afb2d40679f7339ff79
Krishnakant Nagina <naginakrishnakant@gmail.com>
2026-01-27 18:19:15 +0530

    Add Append Method

--- merilist.py
+++ merilist.py
@@ -15,3 +15,24 @@ class MeraList:
     #use magic method for create length check method
     def __len__(self):
         return self.n
+    
+    
+    def append(self, item):
+        #check array is empty or not 
+        if self.size == self.n:
+            #resize the array
+            self.__resize(self.size*2)
+        
+        #append
+        self.A[self.n]=item
+        self.n = self.n + 1
+        
+    def __resize(self, new_capacity):
+        #create a new array with new capacity
+        B = self.__make_array(new_capacity)
+        self.size=new_capacity
+        #copy content of A to B
+        for i in range(self.n):
+            B[i]=self.A[i]
+        #reassign A
+        self.A = B
